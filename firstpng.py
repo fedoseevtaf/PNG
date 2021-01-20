ff8b0c66b21f95bf1e78857c780c69443db2968a
fedoseevtaf <76451152+fedoseevtaf@users.noreply.github.com>
2021-01-20 13:35:50 +0300

    Update firstpng.py

--- firstpng.py
+++ firstpng.py
@@ -1,7 +1,10 @@
+#==============================================================#
 filename = 'firstpng.png'
 
+#==============================================================#
 import zlib
 
+#==============================================================#
 with open(filename, 'wb') as file:
     signature = b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a'
 
@@ -12,7 +15,9 @@ with open(filename, 'wb') as file:
     IHDR = IHDRlen + IHDRname + IHDRdata + IHDRcrc
 
     IDATname = b'IDAT'
-    IDATdata = zlib.compress(b'\x00\xdd\x88\xdd\x44\xff\xff\x00\x44\xff\xff\xdd\x88\xdd')
+    IDATdata = zlib.compress(
+        b'\x00\xdd\x88\xdd\x44\xff\xff\x00\x44\xff\xff\xdd\x88\xdd'
+    )
     IDATlen = len(IDATdata).to_bytes(4, 'big')
     IDATcrc = zlib.crc32(IDATname + IDATdata).to_bytes(4, 'big')
     IDAT = IDATlen + IDATname + IDATdata + IDATcrc
