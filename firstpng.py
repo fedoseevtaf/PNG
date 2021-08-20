545e486191203461c8640eb22066dc4b60676a61
fedoseevtaf <76451152+fedoseevtaf@users.noreply.github.com>
2021-08-20 22:58:20 +0300

    Update firstpng.py

--- firstpng.py
+++ firstpng.py
@@ -1,33 +1,27 @@
-#==============================================================#
-filename = 'firstpng.png'
+from zlib import compress, crc32
 
-#==============================================================#
-import zlib
 
-#==============================================================#
-with open(filename, 'wb') as file:
-    signature = b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a'
+sign = b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a'
 
-    IHDRname = b'IHDR'
-    IHDRdata = b'\x00\x00\x00\x02\x00\x00\x00\x02\x08\x02\x00\x00\x00'
-    IHDRlen = len(IHDRdata).to_bytes(4, 'big')
-    IHDRcrc = zlib.crc32(IHDRname + IHDRdata).to_bytes(4, 'big')
-    IHDR = IHDRlen + IHDRname + IHDRdata + IHDRcrc
+IHDRname = b'IHDR'
+IHDRdata = b'\x00\x00\x00\x02\x00\x00\x00\x02\x08\x02\x00\x00\x00'
+IHDRlen = len(IHDRdata).to_bytes(4, 'big')
+IHDRcrc = crc32(IHDRname + IHDRdata).to_bytes(4, 'big')
 
-    IDATname = b'IDAT'
-    IDATdata = zlib.compress(
-        b'\x00\xdd\x88\xdd\x44\xff\xff\x00\x44\xff\xff\xdd\x88\xdd'
-    )
-    IDATlen = len(IDATdata).to_bytes(4, 'big')
-    IDATcrc = zlib.crc32(IDATname + IDATdata).to_bytes(4, 'big')
-    IDAT = IDATlen + IDATname + IDATdata + IDATcrc
+IHDR = IHDRlen + IHDRname + IHDRdata + IHDRcrc
 
-    IENDname = b'IEND'
-    IENDlen = b'\x00\x00\x00\x00'
-    IENDcrc = zlib.crc32(IENDname).to_bytes(4, 'big')
-    IEND = IENDlen + IENDname + IENDcrc
+IDATname = b'IDAT'
+IDATdata = compress(b'\x00\x00\xff\xff\xff\x00\xff\x00\xff\x00\xff\x00\xff\xff')
+IDATlen = len(IDATdata).to_bytes(4, 'big')
+IDATcrc = crc32(IDATname + IDATdata).to_bytes(4, 'big')
 
-    bytecode = signature + IHDR + IDAT + IEND
+IDAT = IDATlen + IDATname + IDATdata + IDATcrc
 
-    file.write(bytecode)
-    
+IENDname = b'IEND'
+IENDlen = b'\x00\x00\x00\x00'
+IENDcrc = crc32(IENDname).to_bytes(4, 'big')
+
+IEND = IENDlen + IENDname + IENDcrc
+
+with open('first.png', 'wb') as file:
+	file.write(sign + IHDR + IDAT + IEND)
